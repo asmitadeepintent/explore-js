14b552427e6cad416967d6409589778677cabb57
Asmita Wagh <90609433+asmitadeepintent@users.noreply.github.com>
2026-02-07 09:57:40 +0000

    data types

---
+++ basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 20;
+let accountEmail = "asmita.w@gmail.com";
+var accountPassword="12345";
+accountCity = "Pune";
+
+console.log(accountId);
+accountCity="Nagpur";
+console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello"); // we are using Nodejs to run - hence error
+
+console.log(3+9);
+
+let name="Asmita";
+let age=32;
+let isLoggedIn=false;
+
+// number
+// string
+// boolean true/false
+// null stand alone value 
+// undefined if value is not defined. let age;
+// symbol => unique
+// object
+// typeof null => object
+// typeof undeinfed => undefined
+console.log(typeof age,typeof null,typeof undefined);
