34d1916f423430e28bc34bce6f9ef829d129ef3a
Asmita Wagh <90609433+asmitadeepintent@users.noreply.github.com>
2026-02-09 05:54:56 +0000

    comparison and equality

--- basics/03_conversions_operations.js
+++ basics/03_conversions_operations.js
@@ -27,12 +27,26 @@ let stringToBoolean=Boolean(emptyString); // Boolean("") = false
 
 let value = 3;
 let negative = -value;
-console.log(negative);
-console.log(2*2);
-console.log(2**3); // 2 raised to power 3
+// console.log(negative);
+// console.log(2*2);
+// console.log(2**3); // 2 raised to power 3
 
-console.log("1"+2); // 12
-console.log("1"+2+3); //123
-console.log(1+2+"3"); // 33
-console.log(true); // true
-console.log(+true); // 1
+// console.log("1"+2); // 12
+// console.log("1"+2+3); //123
+// console.log(1+2+"3"); // 33
+// console.log(true); // true
+// console.log(+true); // 1
+console.log("2">1);
+console.log(null > 0);
+console.log(null == 0); // null is empty
+console.log(null >= 0); // here null converted to 0 - not predictable
+console.log(undefined ===0); // undefined comparison is always false
+
+// comparison and equality check - both are different methods
+// avoid null comparisons - JS is not predictable in such cases
+
+// strict checks === value + data type
+console.log("2"===2);
+
+
+
